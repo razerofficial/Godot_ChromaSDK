7057569ab318343a94be3d7ee6980438d8b2764f
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-08 14:13:21 -0700

    Setup native hook

--- Godot_ChromaSDK/GodotLibrary.cpp
+++ Godot_ChromaSDK/GodotLibrary.cpp
@@ -1,16 +1,20 @@
 #include "ChromaSDK.h"
+#include <stdio.h>
 
 using namespace godot;
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options * o) {
 	Godot::gdnative_init(o);
+	fprintf(stdout, "godot_gdnative_init\r\n");
 }
 
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options * o) {
 	Godot::gdnative_terminate(o);
+	fprintf(stdout, "godot_gdnative_terminate\r\n");
 }
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void* handle) {
 	Godot::nativescript_init(handle);
 	register_class<ChromaSDK>();
+	fprintf(stdout, "godot_nativescript_init\r\n");
 }
