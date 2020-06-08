651a425c0476d51f862bbf25ae7e0b09a3c9e115
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-08 15:00:02 -0700

    Added custom native method

--- Godot_ChromaSDK/ChromaSDK.cpp
+++ Godot_ChromaSDK/ChromaSDK.cpp
@@ -1,9 +1,11 @@
 #include "ChromaSDK.h"
+#include <stdio.h>
 
 using namespace godot;
 
 void ChromaSDK::_register_methods() {
 	register_method((char*)"_process", &ChromaSDK::_process);
+	register_method((char*)"PluginInit", &ChromaSDK::PluginInit);
 }
 
 void ChromaSDK::_init() {}
@@ -15,4 +17,10 @@ ChromaSDK::~ChromaSDK() {}
 
 void ChromaSDK::_process(float delta)
 {
+}
+
+long godot::ChromaSDK::PluginInit()
+{
+	fprintf(stdout, "PluginInit()\r\n");
+	return 0;
 }
--- Godot_ChromaSDK/ChromaSDK.h
+++ Godot_ChromaSDK/ChromaSDK.h
@@ -16,5 +16,7 @@ namespace godot
 
 		ChromaSDK();
 		~ChromaSDK();
+
+		long PluginInit();
 	};
 }
