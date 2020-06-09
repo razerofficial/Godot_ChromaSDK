abce4fc10b6a2a8300a1d438816bf7ec8d54e4e0
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-08 17:48:07 -0700

    Autogenerate API methods

--- GodotHello/NodeSample.gd
+++ GodotHello/NodeSample.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"../NodeChromaSDK".PluginInit();
+	$"../NodeChromaSDK".Init();
 	pass # Replace with function body.
 
 
